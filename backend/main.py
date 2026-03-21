ecbfb99a32b0ccfd488d3d451eb975b9f9b8c771
Brayan-Sanchez-10 <brayans234569@gmail.com>
2026-03-21 16:16:41 -0500

    Servidor corriendo

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from database import engine, Base
+from routers import usuario, turno, mesa, categoria, producto, pedido, pedido_producto, domicilio, pago
 
 app = FastAPI(title= "Pepiburguer POS")
+
+
+Base.metadata.create_all(bind=engine)
+
+
+app.include_router(usuario.router)
+app.include_router(turno.router)
+app.include_router(mesa.router)
+app.include_router(categoria.router)
+app.include_router(producto.router)
+app.include_router(pedido.router)
+app.include_router(pedido_producto.router)
+app.include_router(domicilio.router)
+app.include_router(pago.router)
 
 @app.get("/")
 def root():
